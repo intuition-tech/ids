80ee9272c45738f09cee191d59f54fddb4dfa9b6
Ramil Karimov <ramil1017@gmail.com>
2024-06-14 17:02:14 +0500

    feat(ids-gallery): Added a web component to use as a gallery.

---
+++ js/gallery.js
@@ -0,0 +1,55 @@
+import PhotoSwipe from './lib/photoswipe/photoswipe.esm.min.js';
+import PhotoSwipeLightbox from './lib/photoswipe/photoswipe-lightbox.esm.min.js';
+
+/**
+ * @param {HTMLDivElement} node
+ */
+const photoSwipeSize = (node) => {
+  // get image tag inside of node
+  if (node.hasAttribute('data-pswp-width') && node.hasAttribute('data-pswp-height')) {
+    return;
+  }
+  /** @type {HTMLImageElement | null} */
+  const img = node.querySelector('img');
+  if (!img) {
+    return;
+  }
+  let i = new Image();
+  i.onload = () => {
+    node.setAttribute('data-pswp-width', i.width.toString());
+    node.setAttribute('data-pswp-height', i.height.toString());
+  };
+  i.src = img.src;
+};
+
+class IdsGallery extends HTMLElement {
+  constructor() {
+    super();
+    this.classList.add('ids__gallery');
+  }
+
+  connectedCallback() {
+    this.lightbox = new PhotoSwipeLightbox({
+      gallery: this,
+      children: 'a',
+      pswpModule: PhotoSwipe,
+      padding: {top: 20, bottom: 40, left: 100, right: 100}
+    });
+
+    this.lightbox.on('uiRegister', () => {
+      if (!this.getAttribute('zoom') && this.lightbox?.pswp?.ui?.uiElementsData?.length) {
+        this.lightbox.pswp.ui.uiElementsData = this.lightbox.pswp.ui.uiElementsData.filter(el => el.name !== 'zoom')
+      }
+    });
+
+    this.lightbox.init();
+
+    this.querySelectorAll('a').forEach(photoSwipeSize)
+  }
+
+  disconnectedCallback() {
+    this.lightbox.destroy();
+  }
+}
+
+window.customElements.define('ids-gallery', IdsGallery);
